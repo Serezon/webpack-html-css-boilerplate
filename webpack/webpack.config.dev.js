fb083b0c3fb839f1ea32fd9cadb85f2ddf263fa0
Pavlo Serediuk <pav.sered@gmail.com>
2019-11-26 23:00:04 +0200

    initial commit

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,49 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js'
+  },
+  devServer: {
+    inline: true
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        }
+      },
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader'
+      },
+      {
+        test:  /\.(css|s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader?sourceMap=true', 'sass-loader']
+      }
+    ]
+  }
+});
